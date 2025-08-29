79af4f4b9d8b796842cb5971d052921ffe8e3c1d
JunayedRafi <junayedrafi8@gmail.com>
2025-08-29 21:54:29 +0600

    Added History Functionality

--- scripts.js
+++ scripts.js
@@ -20,3 +20,43 @@ for (const copyBtn of copyBtns) {
         copyCount.innerText = convertedcopyText + 1;
     })
 }
+
+const callHistory = [];
+const callButtons = document.querySelectorAll(".callButton")
+for (const callButton of callButtons) {
+    callButton.addEventListener("click", function () {
+        const infoCard = this.closest(".card");
+        const emergencyName = infoCard.querySelector(".emergency-name").innerText;
+        const emergencyNum = infoCard.querySelector(".emergency-number").innerText;
+        const strCoins = document.getElementById("coinCountNav")
+        const intCoins = parseInt(strCoins.innerText)
+        if (intCoins < 20) {
+            alert("Low Coin Alert! You need minimum 20 coins to call")
+            return
+        }
+        alert("Calling " + emergencyName + " " + emergencyNum + "...")
+        const totalCoin = intCoins - 20;
+        strCoins.innerText = totalCoin
+
+        const newCallHistory = {
+            name: emergencyName.innerText,
+            number: emergencyNum.innerText,
+            time: new Date().toLocaleTimeString()
+        }
+        callHistory.push(newCallHistory)
+        const historyContainer = document.getElementById("callHistoryContainer")
+        const section = document.createElement("div")
+        section.innerHTML = `
+        <div class="flex bg-[#FAFAFA] justify-between p-3 md:p-4 rounded-lg">
+            <div>
+                <p class="font-bold text-base md:text-lg inter text-black">${newCallHistory.name}</p>
+                <p class="text-base md:text-lg hind-madurai text-[#5C5C5C]">${newCallHistory.number}</p>
+            </div>
+            <div class="content-center">
+                <p class="text-sm md:text-lg hind-madurai text-black">${newCallHistory.time}</p>
+            </div>
+        </div>
+        `
+        historyContainer.appendChild(section)
+    })
+}
